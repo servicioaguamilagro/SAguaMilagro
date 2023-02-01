d3823e730cd5252c42c8272a0c44f771c4bc1027
edhisson97 <60979761+edhisson97@users.noreply.github.com>
2023-01-31 21:01:30 -0500

    restauracion original

--- build.sh
+++ build.sh
@@ -7,7 +7,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-export LC_ALL="es_ES.UTF-8"
-export LC_CTYPE="es_ES.UTF-8"
-sudo dpkg-reconfigure locales
